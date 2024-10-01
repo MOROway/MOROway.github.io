1aaf21fcfa0e4b56b8a57ce0d09178db4bfafc56
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2024-10-01 19:30:58 +0200

    add 10.0 to archive

---
+++ 10years/archive/10.0/src/js/error_handler.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2024 Jonathan Herrmann-Engel
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+window.addEventListener("error", function (event) {
+    if (document.body != null) {
+        document.body.style.background = "#110022";
+        var errorElement = document.querySelector("#error-element");
+        var actionElement = document.querySelector("#error-element-action");
+        if (errorElement == null || actionElement == null) {
+            document.body.innerHTML = '<div id="error-element"><div id="error-element-heading">FATAL ERROR</div><div id="error-element-action">Please report this error… Thanks!</div></div>';
+            errorElement = document.querySelector("#error-element");
+            actionElement = document.querySelector("#error-element-action");
+        }
+        if (errorElement != null && actionElement != null) {
+            var messageElement = document.createElement("div");
+            messageElement.textContent = event.message;
+            messageElement.id = "error-element-message";
+            errorElement.insertBefore(messageElement, actionElement);
+        }
+    }
+});
---
+++ 10years/archive/10.0/src/jsm/common/gui_state.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2024 Jonathan Herrmann-Engel
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+"use strict";
+//GUI STATE
+export var ThreeCameraModes;
+(function (ThreeCameraModes) {
+    ThreeCameraModes["BIRDS_EYE"] = "birds-eye";
+    ThreeCameraModes["FOLLOW_TRAIN"] = "follow-train";
+    ThreeCameraModes["FOLLOW_CAR"] = "follow-car";
+})(ThreeCameraModes || (ThreeCameraModes = {}));
+function validateGuiState(item, test) {
+    if (items.hasOwnProperty(item)) {
+        return items[item].validate(test);
+    }
+    return false;
+}
+function getGuiStates() {
+    var guiState = {};
+    try {
+        guiState = JSON.parse(window.localStorage.getItem("morowayAppGuiState") || "{}");
+    }
+    catch (e) {
+        guiState = {};
+    }
+    return guiState;
+}
+export function getGuiState(item, overrideValue) {
+    if (overrideValue === void 0) { overrideValue = undefined; }
+    var value = getGuiStates()[item];
+    if (overrideValue !== undefined && validateGuiState(item, overrideValue)) {
+        return overrideValue;
+    }
+    else if (validateGuiState(item, value)) {
+        return value;
+    }
+    else if (items.hasOwnProperty(item)) {
+        return items[item].default;
+    }
+    return undefined;
+}
+export function setGuiState(item, value) {
+    var guiState = getGuiStates();
+    if (validateGuiState(item, value)) {
+        guiState[item] = value;
+    }
+    window.localStorage.setItem("morowayAppGuiState", JSON.stringify(guiState));
+}
+var items = {
+    "3d": {
+        default: false,
+        validate: function (test) {
+            return typeof test == "boolean";
+        }
+    },
+    "3d-night": {
+        default: false,
+        validate: function (test) {
+            return typeof test == "boolean";
+        }
+    },
+    "3d-cam-mode": {
+        default: ThreeCameraModes.BIRDS_EYE,
+        validate: function (test) {
+            return Object.values(ThreeCameraModes).includes(test);
+        }
+    },
+    "3d-follow-object": {
+        default: 0,
+        validate: function (test) {
+            return typeof test == "number" && Number.isInteger(test) && test >= 0;
+        }
+    },
+    "3d-rotation-speed": {
+        default: 50,
+        validate: function (test) {
+            return typeof test == "number" && !Number.isNaN(test) && test >= 0 && test <= 100;
+        }
+    },
+    "demo-random": {
+        default: false,
+        validate: function (test) {
+            return typeof test == "boolean";
+        }
+    }
+};
