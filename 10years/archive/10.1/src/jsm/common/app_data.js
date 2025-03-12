676db5c472e45750e26cc3cda1e20f847c88b65d
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2025-03-12 22:08:57 +0100

    add 10.1 to archive

---
+++ 10years/archive/10.1/src/jsm/common/app_data.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2024 Jonathan Herrmann-Engel
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+"use strict";
+import { deepFreeze } from "./js_objects.js";
+//Placeholders are set by build-script, type error is therefore intentional
+var APP_DATA = {
+    version: {
+        major: 10,
+        minor: 1,
+        patch: 2,
+        beta: 0,
+        date: {
+            year: 2024,
+            month: 12,
+            day: 12
+        }
+    },
+    platform: "web",
+    debug: false
+};
+deepFreeze(APP_DATA);
+export { APP_DATA };
+//LOCAL APP DATA COPY
+export function getLocalAppDataCopy() {
+    return JSON.parse(window.localStorage.getItem("morowayAppData") || "null");
+}
+export function setLocalAppDataCopy() {
+    window.localStorage.setItem("morowayAppData", JSON.stringify(APP_DATA));
+}
---
+++ 10years/archive/10.1/src/jsm_platform/scripting.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2024 Jonathan Herrmann-Engel
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+"use strict";
+import { followLink, LINK_STATE_INTERNAL_HTML } from "./common/follow_links.js";
+import { APP_DATA } from "../jsm/common/app_data.js";
+import { showServerNote } from "../jsm/common/web_tools.js";
+import { getString } from "../jsm/common/string_tools.js";
+import { setSettingsHTML } from "../jsm/common/settings.js";
+import { notify, NOTIFICATION_PRIO_DEFAULT } from "../jsm/common/notify.js";
+import { getMode } from "../jsm/scripting.js";
+document.addEventListener("moroway-app-ready", function () {
+    if (getMode() == "normal") {
+        showServerNote(document.querySelector("#server-note"));
+    }
+    setSettingsHTML(document.querySelector("#settings-inner"), false);
+});
+document.addEventListener("moroway-app-ready-notification", function (event) {
+    var eventCustom = event;
+    var minHeight = 0;
+    if (eventCustom.detail && eventCustom.detail.notifyMinHeight) {
+        minHeight = eventCustom.detail.notifyMinHeight;
+    }
+    notify("#canvas-notifier", getString("appScreenHasLoaded", "."), NOTIFICATION_PRIO_DEFAULT, 4000, function () {
+        followLink("help", "_blank", LINK_STATE_INTERNAL_HTML);
+    }, getString("generalTitleHelpScreen", "", "upper"), minHeight);
+});
+document.addEventListener("moroway-app-update-notification", function (event) {
+    var eventCustom = event;
+    var minHeight = 0;
+    if (eventCustom.detail && eventCustom.detail.notifyMinHeight) {
+        minHeight = eventCustom.detail.notifyMinHeight;
+    }
+    notify("#canvas-notifier", getString("generalNewVersion", "!", "upper"), NOTIFICATION_PRIO_DEFAULT, 7000, function () {
+        followLink("whatsnew/#newest", "_blank", LINK_STATE_INTERNAL_HTML);
+    }, getString("appScreenFurtherInformation", "", "upper"), minHeight);
+});
+document.addEventListener("moroway-app-keep-screen-alive", function (event) {
+    var mode = getMode();
+    if (mode == "online" || mode == "demo") {
+        var eventCustom = event;
+        if (eventCustom.detail) {
+            if (eventCustom.detail.acquire) {
+                try {
+                    navigator.wakeLock.request("screen");
+                }
+                catch (error) {
+                    if (APP_DATA.debug) {
+                        console.log("Wake-Lock-Error:", error);
+                    }
+                }
+            }
+        }
+    }
+});
